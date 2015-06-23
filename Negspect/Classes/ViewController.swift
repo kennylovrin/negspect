f974ace462f8338621eae2e92a58e6eec5f5d46b
Kenny Lövrin <kenny@Kennys-MacBook-Pro.local>
2015-06-23 18:57:41 +0200

    initial commit

---
+++ Negspect/Classes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Negspect
+//
+//  Created by Kenny Lövrin on 23/06/15.
+//  Copyright © 2015 Kenny Lövrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
